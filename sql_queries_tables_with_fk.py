1c6ca068402fa1a3eccd671372495c2c2bec7aca
piotrulanowski <piotr.ulanowski@gmail.com>
2021-01-14 22:57:40 +0100

    14/01/2021

--- sql_queries_tables_with_fk.py
+++ sql_queries_tables_with_fk.py
@@ -9,7 +9,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays(\
-                songplay_id integer,\
+                songplay_id integer CONSTRAINT pk_songplays PRIMARY KEY,\
                 start_time timestamp UNIQUE,\
                 user_id integer UNIQUE,\
                 level varchar,\
@@ -20,46 +20,50 @@ songplay_table_create = ("CREATE TABLE IF NOT EXISTS songplays(\
                 user_agent varchar);")
 
 artist_table_create = ("CREATE TABLE IF NOT EXISTS artists(\
-                artist_id varchar,\
+                artist_id varchar CONSTRAINT pk_artists PRIMARY KEY,\
                 name varchar,\
                 location varchar,\
                 latitude float,\
-                longitude float);")
+                longitude float,\
+                CONSTRAINT fk_artists FOREIGN KEY(artist_id)\
+                    REFERENCES songplays(artist_id));")
 
 user_table_create = ("CREATE TABLE IF NOT EXISTS users(\
-                user_id varchar,\
+                user_id integer CONSTRAINT pk_users PRIMARY KEY,\
                 first_name varchar,\
                 last_name varchar,\
                 gender varchar,\
-                level varchar);")
+                level varchar,\
+                CONSTRAINT fk_users FOREIGN KEY(user_id)\
+                    REFERENCES songplays(user_id));")
 
 song_table_create = ("CREATE TABLE IF NOT EXISTS songs(\
-                song_id varchar,\
+                song_id varchar CONSTRAINT pk_songs PRIMARY KEY,\
                 title varchar,\
                 artist_id varchar,\
                 year integer,\
-                duration float);")
+                duration float,\
+                CONSTRAINT fk_songs FOREIGN KEY(song_id)\
+                    REFERENCES songplays(song_id));")
 
 time_table_create = ("CREATE TABLE IF NOT EXISTS time(\
-                start_time timestamp,\
+                start_time timestamp CONSTRAINT pk_time PRIMARY KEY,\
                 hour time,\
                 day integer,\
                 week integer,\
                 month integer,\
                 year integer,\
-                weekday integer);")
+                weekday integer,\
+                CONSTRAINT fk_time FOREIGN KEY(start_time)\
+                    REFERENCES songplays(start_time));")
                 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
 """)
 
-user_table_insert = ("INSERT INTO users (user_id,\
-                        first_name,\
-                        last_name,\
-                        gender,\
-                        level)\
-                        VALUES (%s, %s, %s, %s, %s);")
+user_table_insert = ("""
+""")
 
 song_table_insert =  ("INSERT INTO songs (song_id,\
                         title,\
@@ -68,12 +72,8 @@ song_table_insert =  ("INSERT INTO songs (song_id,\
                         duration)\
                         VALUES (%s, %s, %s, %s, %s);")
 
-artist_table_insert = ("INSERT INTO artists (artist_id,\
-                        name,\
-                        location,\
-                        latitude,\
-                        longitude)\
-                        VALUES (%s, %s, %s, %s, %s);")
+artist_table_insert = ("""
+""")
 
 
 time_table_insert = ("""
